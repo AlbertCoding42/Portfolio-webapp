72aa7d8f364137d43a20f4450817d83de6dc7c2d
AAtkinson <Aatkinson@academytrainee.com>
2023-10-11 16:17:26 +0100

    Initial commit

---
+++ my-react-app - Bertie/src/App.js
@@ -0,0 +1,14 @@
+import './css/normalize.css';
+import './css/webflow.css';
+import './css/styles.css';
+
+import './js/webflow.js';
+
+
+function App() {
+  return (
+      <></>
+  );
+}
+
+export default App;
